cf223f5169f5de4fbace7a8ede39b52cd0965aa4
TheekshaniPramodya <theekshaniph@gmail.com>
2025-07-13 16:11:32 +0530

    kotlin

--- app/src/main/java/com/example/activity1/ui/theme/lesthree.kt
+++ app/src/main/java/com/example/activity1/ui/theme/lesthree.kt
@@ -1,9 +1,14 @@
 package com.example.activity1.ui.theme
 
-data class Servers(val name : String, val ip: String,val location :String )
+data class Serverr(val name : String, val ip: String,val location:String )
 fun main(){
-    val serverone = listOf(Servers("japan","123.123.123.123","japan"),Servers("india","123.123.123.123","india"),Servers("uk","123.123.123.123","uk"))
-    for((index,server)in serverone.withIndex()){
+    val servers = listOf(Serverr("japan","123.123.123.123","japan"),Serverr("india","123.123.123.123","india"),Serverr("uk","123.123.123.123","uk"))
+    for((index,server)in servers.withIndex()){
         println("${index+1}.$server ")
+        connect(server)
     }
+}
+fun  connect(server:Serverr){
+    println("Connecting to ${server.location} \n" +
+            "server at ${server.ip}")
 }
